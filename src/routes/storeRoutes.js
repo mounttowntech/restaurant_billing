b9e43f79159d76bf4d292979b6129adce2da678a
vikash-mounttowntech <mounttowntech@gmail.com>
2026-08-12 17:44:29 +0530

    POSBilling, Store completed

--- src/routes/storeRoutes.js
+++ src/routes/storeRoutes.js
@@ -18,45 +18,24 @@ const {
 
 // Create store
 // No authentication required for initial setup
-router.post(
-  "/create",
-  createStore
-);
+router.post("/create", createStore);
 
 // Get all stores
-router.get(
-  "/all",
-  getAllStores
-);
+router.get("/all", getAllStores);
 
 // Get store by ID
-router.get(
-  "/:id",
-  getStoreById
-);
+router.get("/:id", getStoreById);
 
 // Update store
-router.put(
-  "/update/:id",
-  updateStore
-);
+router.put("/update/:id", updateStore);
 
 // Delete store
-router.delete(
-  "/delete/:id",
-  deleteStore
-);
+router.delete("/delete/:id", deleteStore);
 
 // Restore store
-router.patch(
-  "/restore/:id",
-  restoreStore
-);
+router.patch("/restore/:id", restoreStore);
 
 // Toggle status
-router.patch(
-  "/toggle-status/:id",
-  toggleStoreStatus
-);
+router.patch("/toggle-status/:id", toggleStoreStatus);
 
 module.exports = router;
